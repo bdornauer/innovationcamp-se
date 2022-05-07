a25826816fed77755fbcca601eb1b7d1b058caa3
Benedikt Dornauer <benedikt.dornauer@outlook.at>
2022-05-07 08:46:00 +0200

    Create sonarquobeExample.js

---
+++ cleanCode_examples/sonarquobeExample.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+    if(x>y)
+        return x-y
+    else
+        return y-x
+
+    return "a different result"; //nicht erreichbar
+}
+
+function mult(x,y){
+    if(true)
+        return x*y;
+}
+
+function f(x,y){
+    return 45;
+}
+
+//
+mult(3,3,4)
+
